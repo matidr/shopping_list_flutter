128ab370c428a48ec58a657ae36e7083c3777fd3
matidr <matias.dirusso@gmail.com>
2025-08-27 22:33:39 -0300

    Save form and send new item to the previous list screen

--- lib/screens/groceries_list.dart
+++ lib/screens/groceries_list.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:shopping_list/data/dummy_items.dart';
+import 'package:shopping_list/models/grocery_item.dart';
 import 'package:shopping_list/widgets/grocery_item.dart';
 import 'package:shopping_list/widgets/new_item.dart';
 
@@ -13,25 +13,32 @@ class GroceriesListScreen extends StatefulWidget {
 }
 
 class _GroceriesListScreenState extends State<GroceriesListScreen> {
+  final List<GroceryItem> _groceryItems = [];
+
+  void _addItem() async {
+    final newItem = await Navigator.of(
+      context,
+    ).push<GroceryItem>(MaterialPageRoute(builder: (ctx) => const NewItem()));
+
+    if (newItem == null) {
+      return;
+    }
+
+    setState(() {
+      _groceryItems.add(newItem);
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text("Your Groceries"),
-        actions: [
-          IconButton(
-            onPressed: () {
-              Navigator.of(
-                context,
-              ).push(MaterialPageRoute(builder: (ctx) => const NewItem()));
-            },
-            icon: const Icon(Icons.add),
-          ),
-        ],
+        actions: [IconButton(onPressed: _addItem, icon: const Icon(Icons.add))],
       ),
       body: ListView.builder(
-        itemCount: groceryItems.length,
-        itemBuilder: (context, index) => groceryItems
+        itemCount: _groceryItems.length,
+        itemBuilder: (context, index) => _groceryItems
             .map((groceryItem) => GroceryListItem(item: groceryItem))
             .toList()[index],
       ),
