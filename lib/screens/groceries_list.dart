dfc9f4b5df0b9283751529dbd6a28258e17d7e14
matidr <matias.dirusso@gmail.com>
2025-08-27 22:23:51 -0300

    Add new form with validations

--- lib/screens/groceries_list.dart
+++ lib/screens/groceries_list.dart
@@ -1,14 +1,34 @@
 import 'package:flutter/material.dart';
 import 'package:shopping_list/data/dummy_items.dart';
 import 'package:shopping_list/widgets/grocery_item.dart';
+import 'package:shopping_list/widgets/new_item.dart';
 
-class GroceriesListScreen extends StatelessWidget {
+class GroceriesListScreen extends StatefulWidget {
   const GroceriesListScreen({super.key});
 
   @override
+  State<StatefulWidget> createState() {
+    return _GroceriesListScreenState();
+  }
+}
+
+class _GroceriesListScreenState extends State<GroceriesListScreen> {
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("Your Groceries")),
+      appBar: AppBar(
+        title: const Text("Your Groceries"),
+        actions: [
+          IconButton(
+            onPressed: () {
+              Navigator.of(
+                context,
+              ).push(MaterialPageRoute(builder: (ctx) => const NewItem()));
+            },
+            icon: const Icon(Icons.add),
+          ),
+        ],
+      ),
       body: ListView.builder(
         itemCount: groceryItems.length,
         itemBuilder: (context, index) => groceryItems
